01710926f04fb00ba06b2120f171c29adfd7d881
Jack Pee <mcinvasion2016@gmail.com>
2020-10-10 15:01:40 -0700

    make travis work pls

--- bot.py
+++ bot.py
@@ -1,7 +1,35 @@
 import discord
 import json
 import os
+import argparse
 from discord.ext import commands
+
+def parse_cmd_arguments():  # allows for arguments
+    parser = argparse.ArgumentParser(description="Discord-Selfbot")
+    parser.add_argument("-test", "--test-run",  # test run flag for Travis
+                        action="store_true",
+                        help="Makes the bot quit before trying to log in")
+    parser.add_argument("--force-mac",  # Allows for Testing of mac related code
+                        action="store_true",
+                        help="Forces to run the Mac checks")
+    parser.add_argument("--reset-config",  # Allows for Testing of mac related code
+                        action="store_true",
+                        help="Reruns the setup")
+    parser.add_argument("-s", "--silent",  # Allows for Testing of mac related code
+                        action="store_true",
+                        help="Supresses all errors")
+    return parser
+
+args = parse_cmd_arguments().parse_args()
+_test_run = args.test_run
+_force_mac = args.force_mac
+_reset_cfg = args.reset_config
+_silent = args.silent
+_force_admin = False
+
+if _test_run:
+    print("Quitting: test run")
+    exit(0)
 
 with open("config.json", "r") as f:
     config = json.load(f)
